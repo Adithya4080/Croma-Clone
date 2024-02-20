fa95fac9d6acc779c3524df3cbb463d49df8a4ab
adithya4080 <adithyas712@gmail.com>
2024-02-20 08:27:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import Accessories from './components/mainPages/accessoriesPage/Accessories';
 import HomePage from './components/mainPages/homePage/HomePage';
 import Signup from './components/login/Signup';
 import Login from './components/login/Login';
+import React, { useState, useEffect } from 'react';
+
+export const UserContext = React.createContext();
 
 
 function App() {
+    const [userData, setUserData] = useState({});
+    const updateUserData = (action) => {
+        switch (action.type) {
+            case "LOGOUT":
+                setUserData(null);
+                localStorage.clear();
+                break;
+            case "LOGIN":
+                setUserData(action.payload);
+                break;
+            default:
+                break;
+        }
+    };
+
+    useEffect(() => {
+        setUserData(JSON.parse(localStorage.getItem("user_data")));      
+    }, []);
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/accessories' element={<Accessories />} />
-          <Route path='/auth/signup' element={<Signup />} />
-          <Route path='/auth/login' element={<Login />} />
-        </Routes>
-      </Router>
-    </>
+      <>
+          <UserContext.Provider value={{ userData, updateUserData }}>
+              <Router>
+                  <Routes>
+                      <Route path='/' element={<HomePage />} />
+                      <Route path='/accessories' element={<Accessories />} />
+                      <Route path='/auth/create' element={<Signup />} />
+                      <Route path='/auth/login' element={<Login />} />
+                  </Routes>
+              </Router>
+          </UserContext.Provider>
+      </>
   );
 }
 
--- src/components/navbar/MenuBar.jsx
+++ src/components/navbar/MenuBar.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import { HiOutlineBars3 } from 'react-icons/hi2'
+import React, { useState } from 'react';
+import { IoMdClose } from 'react-icons/io';
+import { HiOutlineBars3 } from 'react-icons/hi2';
+import DropdownTop from '../menubarDropdown/DropdownTop';
+import DropdownBottom from '../menubarDropdown/DropdownBottom';
 
-function MenuBar() {
-    return (
-        <div className='flex items-center cursor-pointer'>
-            <HiOutlineBars3 className='text-3xl mr-1'/>
-            <small>Menu</small>
+const MenuBar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className='relative'>
+      {isDropdownOpen && (
+        <div
+          className='fixed inset-0 bg-gray-700 opacity-60'
+          onClick={toggleDropdown}
+        ></div>
+      )}
+
+      <div
+        className='flex items-center cursor-pointer relative'
+        onClick={toggleDropdown}
+      >
+        {isDropdownOpen ? (
+          <IoMdClose className='text-3xl mr-1' />
+        ) : (
+          <HiOutlineBars3 className='text-3xl mr-1' />
+        )}
+        <small>Menu</small>
+        <div className='flex flex-col'>
+            <DropdownTop isDropdownOpen={isDropdownOpen} />
+            <DropdownBottom isDropdownOpen={isDropdownOpen} />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default MenuBar
+export default MenuBar;
+
+
