98634bcdf103a155f6625debf2cf061542b138f3
adithya4080 <adithyas712@gmail.com>
2024-02-28 13:00:26 +0530

    update

--- src/components/mainPages/accessoriesPage/accessories/AccessoriesPage.jsx
+++ src/components/mainPages/accessoriesPage/accessories/AccessoriesPage.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react';
+
+import React, { useContext, useState } from 'react';
+import LazyLoad from 'react-lazy-load';
 import accessories from '../../../../data/accessories/Accessories.json';
 import { FaScrewdriverWrench } from "react-icons/fa6";
 import { AiFillStar } from 'react-icons/ai';
@@ -6,6 +8,7 @@ import { Cart } from '../../../context/Context';
 import { FilterContext } from '../../../filter/FilterContext';
 
 function AccessoriesPage() {
+    const [itemsToShow, setItemsToShow] = useState(9);
     const { addToCart } = useContext(Cart)
     
     const addCart = (item) => {
@@ -21,38 +24,53 @@ function AccessoriesPage() {
         (selectedModes.length === 0 || selectedModes.includes(item.delivery))
     );
 
+    const showMoreItems = () => {
+        setItemsToShow(itemsToShow + 6)
+    }
+
     return (
-        <div className='wrapper mt-10 grid grid-cols-3 gap-4'style={{ marginTop: '60px' }}>
-            {filteredAccessories.map((items) => (
-                <div key={items.id}>
-                    <div className='bg-zinc-700 rounded-lg w-10/12 h-3/5'>
-                        <img src={items.img} alt={items.name} className='p-14' />
-                    </div>
-                    <div>
-                        <h2 className='title mt-5 font-bold'>{items.title}</h2>
-                    </div>
-                    <div className='flex mr-1 mb-2'>
-                        <div className='text-teal-500 flex items-center mr-1'>
-                            <p className='mr-1'>{items.rating}</p>
-                            <p><AiFillStar /></p>
+        <div className='wrapper mt-10 'style={{ marginTop: '60px' }}>
+            <div className='grid grid-cols-3 gap-4'>
+                {filteredAccessories.slice(0, itemsToShow).map((items) => (
+                    <div key={items.id}>
+                        <div >
+                        <LazyLoad className='bg-zinc-700 rounded-lg w-10/12 h-3/5'  threshold={0.95}>
+                            <img src={items.img} alt={items.name} className='p-14' loading='lazy' />
+                        </LazyLoad>
                         </div>
-                        <p>{items.review}</p>
+                        <div>
+                            <h2 className='title mt-5 font-bold'>{items.title}</h2>
+                        </div>
+                        <div className='flex mr-1 mb-2'>
+                            <div className='text-teal-500 flex items-center mr-1'>
+                                <p className='mr-1'>{items.rating}</p>
+                                <p><AiFillStar /></p>
+                            </div>
+                            <p>{items.review}</p>
+                        </div>
+                        <div className='flex items-center mb-3'>
+                            <h3 className='font-bold text-2xl mr-3'>₹{items.newPrice}</h3>
+                            <del className='mr-2 text-sm'>{items.prevPrice}</del>
+                            <small className='mr-2 text-gray-400'>{items.saving}</small>
+                            <button className='border border-gray-700 rounded-sm text-xs px-1 font-semibold'>{items.discount}</button>
+                        </div>
+                        <div className='flex items-center'>
+                            <FaScrewdriverWrench />
+                            <p className='ml-2 text-sm'>{items.delivery}</p>
+                        </div>
+                        <div className='mt-3'>
+                            <button onClick={() => {addCart(items)}} className='bg-teal-500 text-black rounded-lg py-1 px-3 hover:text-white'>Add to Cart</button>
+                        </div>
                     </div>
-                    <div className='flex items-center mb-3'>
-                        <h3 className='font-bold text-2xl mr-3'>₹{items.newPrice}</h3>
-                        <del className='mr-2 text-sm'>{items.prevPrice}</del>
-                        <small className='mr-2 text-gray-400'>{items.saving}</small>
-                        <button className='border border-gray-700 rounded-sm text-xs px-1 font-semibold'>{items.discount}</button>
+                ))}
+            </div>
+                {itemsToShow < filteredAccessories.length &&(
+                    <div className='flex justify-center items-center'>
+                        <button onClick={showMoreItems} className='text-white border border-none bg-teal-900 px-8  rounded-lg py-2 mt-10'>
+                            View More
+                        </button>
                     </div>
-                    <div className='flex items-center'>
-                        <FaScrewdriverWrench />
-                        <p className='ml-2 text-sm'>{items.delivery}</p>
-                    </div>
-                    <div className='mt-3'>
-                        <button onClick={() => {addCart(items)}} className='bg-teal-500 text-black rounded-lg py-1 px-3 hover:text-white'>Add to Cart</button>
-                    </div>
-                </div>
-            ))}
+                )}
         </div>
     )
 }
